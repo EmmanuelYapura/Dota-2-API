3cca94404fb39aab6418ef024d7218cb62f91147
Emmanuel <eyapura96@gmail.com>
2022-04-07 22:54:53 -0300

    carga funcion  para peticiones fetch

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const url = 'https://api.opendota.com'
+
+async function getFetch(link) {
+    try {
+        let responseJson = await fetch(link)
+        let response = await responseJson.json()
+        return response
+    } catch (error) {
+        console.error("Hubo un error en la peticion: ", error)
+    }
+}
